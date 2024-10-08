464a4fa831f85ef36779d91d8c222316bd566576
ronny-j <raninitaly@gmail.com>
2024-10-08 12:11:05 +0200

    add

---
+++ lab00/cmake-build-debug/CMakeFiles/lab00.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab00.dir/main.c.o"
+  "CMakeFiles/lab00.dir/main.c.o.d"
+  "lab00"
+  "lab00.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
